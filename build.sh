990ebe091ef5718c7e762f2d0fd4b3f6ec0826cf
Jmart2424 <admin@heydaydigitalmedia.com>
2024-12-09 07:49:06 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Create a virtual environment
 python3 -m venv .venv
+
+# Activate the virtual environment
 source .venv/bin/activate
+
+# Upgrade pip to the latest version
+python3 -m pip install --upgrade pip
+
+# Install the required dependencies
 python3 -m pip install -r requirements.txt
+
+# Deactivate the virtual environment
+deactivate
